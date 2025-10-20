9b298ccca731719c327c8e94425720d5e7777ab5
Kirill Kulikovskii <cyril.kulikovsky@gmail.com>
2025-10-20 11:50:04 +0100

    mobile UI rework (5)

--- script.js
+++ script.js
@@ -140,6 +140,21 @@ function submitScore(){
 
 }
 
+document.addEventListener('touchstart', function preventZoom(e) {
+  if (e.touches.length > 1) {
+    e.preventDefault();
+  } }, { passive: false });
+
+let lastTouchEnd = 0;
+document.addEventListener('touchend', function preventDoubleTap(e) {
+  const now = new Date().getTime();
+  if (now - lastTouchEnd <= 300) {
+    e.preventDefault(); 
+  }
+  lastTouchEnd = now;
+}, false);
+
+
 document.getElementById("commentForm").addEventListener("submit", handleCommentSubmit);
 getAdviceBtn.addEventListener('click',fetchAdvice)
 document.addEventListener("DOMContentLoaded", fetchComments);
